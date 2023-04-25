2039d7cfb147c4bff31f7b3979d947f6e6613f28
princystephany <stephanyben6@gmail.com>
2023-04-25 03:27:48 -0700

    My first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Sample from './component/Sample.jsx'
 
 function App() {
+
+  let name='Princy';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+<h1>
+  First React Application
+<Sample/>
+
+</h1>
+<p>{name}</p>
+   </div>  
   );
 }
 
---
+++ src/component/Sample.jsx
@@ -0,0 +1,12 @@
+function Sample() {
+  
+    return (
+     <>
+  <h1>
+    First jsx component
+  </h1>
+      </>  
+    );
+  }
+  
+  export default Sample;
