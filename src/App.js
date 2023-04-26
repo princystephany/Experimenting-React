1b8bbfb47fa9d88b5c7244c7336570f311ee10ee
princystephany <stephanyben6@gmail.com>
2023-04-25 23:19:13 -0700

    Props intro

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import {Sample,Sample2} from './component/Sample.jsx'
 
 import Card from './component/Card.jsx'
+
+import image from './images/Nokia.jpg'
  
 function App() {
 
@@ -16,7 +18,11 @@ function App() {
   First React Application
 <Sample/>
 <Sample2/>
-<Card/>
+<Card brandimage={image} brandname="Samsung" brandprice="14,000"></Card>
+<Card brandname="Vivo" brandprice="14,000"></Card>
+<Card brandname="Iphone" brandprice="14,000"></Card>
+<Card brandname="Poco" brandprice="14,000"></Card>
+<Card brandname="Moto" brandprice="14,000"></Card>
 
  </h1>
 <p>{name}</p>
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
- function Card() {
+ function Card(props) {
   
     return (
      <>
-  <div class="card" >
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div className="card" style={{width: "18rem"}}>
+  <img src="{props.brandimage}" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{props.brandname}</h5>
+    <p className="card-text">Some </p>
+    <a href="" className="btn btn-primary">Go somewhere</a>
+    <p>{props.brandprice}</p>
   </div>
 </div>
-<Card/>
+ 
       </>  
     );
   }
    export default Card;
-n
+ 
